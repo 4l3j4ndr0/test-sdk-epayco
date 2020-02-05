0f7b86c01714e5293048718d1a2cac5348f36b2e
ALEJANDRO <alejandro.casta3447@gmail.com>
2020-02-04 22:42:43 -0500

    initial commit

---
+++ test-sdk/Controllers/EpaycoController.cs
@@ -0,0 +1,222 @@
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Web;
+using EpaycoSdk.Models;
+using EpaycoSdk.Models.Bank;
+using EpaycoSdk.Models.Plans;
+using EpaycoSdk.Models.Subscriptions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test_sdk.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EpaycoController : ControllerBase
+    {
+        
+        EpaycoSdk.Epayco epayco = new EpaycoSdk.Epayco(
+            "d7aae6aebc15f77bb434f42c26450d6e",
+            "0242dfaf1e866bb2830f8915b4f1efdd",
+            "ES",
+            true); 
+        [HttpGet]
+        public IActionResult Get()
+        {
+            
+            // return Ok(AESEncrypt("1022", "0242dfaf1e866bb2830f8915b4f1efdd"));
+            return Ok("ePayco sdk .Net");
+        }
+
+        [HttpGet, Route("create/token")]
+        public IActionResult CreateToken()
+        {
+            TokenModel token = epayco.CreateToken(
+                "4575623182290326",
+                "2019",
+                "12",
+                "123");
+            return Ok(token);
+        }
+        
+        [HttpGet, Route("create/customer")]
+        public IActionResult CreateCustomer()
+        {
+            CustomerCreateModel customer = epayco.CustomerCreate(
+                "vDCnfNXzepWb7W8Ht",
+                "alejo desde .net",
+                ".net",
+                "alejonetsdk898@gmail.com",
+                true,
+                "caldas",
+                "calle falsa",
+                "3032546",
+                "3146254435");
+            return Ok(customer);
+        }
+        
+        [HttpGet, Route("find/customer")]
+        public IActionResult FindCustomer()
+        {
+            CustomerFindModel customer = epayco.FindCustomer("cLhn4cc4n8fCCLR4A");
+            return Ok(customer);
+        }
+        [HttpGet, Route("customer/update")]
+        public IActionResult CustomerUpdate()
+        {
+            CustomerEditModel customer = epayco.CustomerUpdate("cLhn4cc4n8fCCLR4A", "pepito perez");
+            return Ok(customer);
+        }
+        
+        [HttpGet, Route("customer/token/delete")]
+        public IActionResult CustomerTokenDelete()
+        {
+            CustomerTokenDeleteModel customer = epayco.CustomerDeleteToken(
+                "visa", 
+                "457562******0326", 
+                "cLhn4cc4n8fCCLR4A");
+            return Ok(customer);
+        }
+        
+        [HttpGet, Route("customers")]
+        public IActionResult GetAllCustomers()
+        {
+            CustomerListModel customer = epayco.CustomerGetList();
+            return Ok(customer);
+        }
+        
+        /*
+         * PLANS
+         */
+        [HttpGet, Route("plans/create")]
+        public IActionResult CreatePlan()
+        {
+            CreatePlanModel plan = epayco.PlanCreate(
+                "pago_.net_sdk_prueba", 
+                "prueba 3",
+                "prueba sdk .net",
+                5000,
+                "cop",
+                "month",
+                1,
+                0);
+            return Ok(plan);
+        }
+        
+        [HttpGet, Route("plans/get")]
+        public IActionResult GetPlan()
+        {
+            FindPlanModel plan = epayco.GetPlan("mi_plan_2_sdk.net");
+            return Ok(plan);
+        }
+        
+        [HttpGet, Route("plans/get/all")]
+        public IActionResult GetAllPlans()
+        {
+            FindAllPlansModel plan = epayco.GetAllPlans();
+            return Ok(plan);
+        }
+        
+        [HttpGet, Route("plans/remove")]
+        public IActionResult RemovePlan()
+        {
+            RemovePlanModel plan = epayco.RemovePlan("00");
+            return Ok(plan);
+        }
+        
+        /*
+         * SUBSCRIPTIONS
+         */
+        [HttpGet, Route("subscriptions/create")]
+        public IActionResult CreateSubscription()
+        {
+            CreateSubscriptionModel subscription = epayco.SubscriptionCreate("prueba_charge_subscription_.net",
+                "eBk2d4ubDAXETsg2e",
+                "2CeoeeoWZBCZRAJ7p",
+                "cc",
+                "1026150802",
+                "https://holapapi.com.co",
+                "POST");
+            return Ok(subscription);
+        }
+        
+        [HttpGet, Route("subscriptions/find")]
+        public IActionResult FindSubscription()
+        {
+            FindSusbscriptionModel subscription = epayco.getSubscription(
+                "KY7J8KvAa7Xu5hze9");
+            return Ok(subscription);
+        }
+        
+        [HttpGet, Route("subscriptions/all")]
+        public IActionResult GetSubscription()
+        {
+            AllSubscriptionModel subscription = epayco.getAllSubscription();
+            return Ok(subscription);
+        }
+        
+        [HttpGet, Route("subscriptions/cancel")]
+        public IActionResult CancelSubscription()
+        {
+            CancelSubscriptionModel subscription = epayco.cancelSubscription("KY7J8KvAa7Xu5hze9");
+            return Ok(subscription);
+        }
+        
+        [HttpGet, Route("subscriptions/charge")]
+        public IActionResult ChargeSubscription()
+        {
+            ChargeSubscriptionModel subscription = epayco.ChargeSubscription(
+                "prueba_charge_subscription_.net2",
+                "yf9zh6xkFjiS2yvM9",
+                "3m7gqWQLoceaCZRvs",
+                "CC",
+                "1026150902",
+                "0.0.0.0",
+                "calle falsa",
+                "3146254435",
+                "3146254435");
+            return Ok(subscription);
+        }
+        
+        [HttpGet, Route("pse/create")]
+        public IActionResult CreatePse()
+        {
+            string response = epayco.BankCreate(
+                "1022",
+                "1472050778",
+                "pago de pruebas",
+                "10000",
+                "0",
+                "0",
+                "COP",
+                "0",
+                "CC",
+                "1026150902",
+                "alejandro",
+                "castañeda",
+                "alejandro.casta3447@gmail.com",
+                "CO",
+                "3146254435",
+                "https:/secure.payco.co/restpagos/testRest/endpagopse.php",
+                "https:/secure.payco.co/restpagos/testRest/endpagopse.php",
+                "GET");
+            return Ok(response);
+        }
+        
+        [HttpGet, Route("get/transaction")]
+        public IActionResult GetTransaction()
+        {
+            TransactionModel transaction = epayco.GetTransaction("101867821569978420");
+            return Ok(transaction);
+        }
+        
+        [HttpGet, Route("get/banks")]
+        public IActionResult GetBanks()
+        {
+            BanksModel banks = epayco.GetBanks();
+            return Ok(banks);
+        }
+         
+    }
+}
